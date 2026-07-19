5d0593317defc2bd1072a6383194744569556dd5
Ilia Fedin <infedinwork@gmail.com>
2026-07-19 16:08:33 +0300

    feat(errors): add custom exceprions for error handling

---
+++ backend/src/TimescaleDataProcessor.Api/Exceptions/BusinessRuleViolationException.cs
@@ -0,0 +1,11 @@
+﻿namespace TimescaleDataProcessor.Api.Exceptions
+{
+    public class BusinessRuleViolationException : AppException
+    {
+        public override string ErrorCode => "business_rule_violation";
+
+
+        public BusinessRuleViolationException(string message) 
+            : base(message) { }
+    }
+}
---
+++ backend/src/TimescaleDataProcessor.Api/Exceptions/FileFormatException.cs
@@ -0,0 +1,10 @@
+﻿namespace TimescaleDataProcessor.Api.Exceptions
+{
+    public class FileFormatException : AppException
+    {
+        public override string ErrorCode => "file_format_error";
+
+        public FileFormatException(string message) 
+            : base(message) { }
+    }
+}
