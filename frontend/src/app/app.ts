9ae88a118bc1a8ae71904d016729bd42d03cd74c
Ilia Fedin <infedinwork@gmail.com>
2026-07-21 17:27:33 +0300

    feat: register all components in app component and add sections in app.html

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { IntegralResults } from './components/integral-results/integral-results';
+import { LatestValues } from './components/latest-values/latest-values';
+import { ImportValues } from './components/import-values/import-values';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [IntegralResults, LatestValues, ImportValues],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+
 }
